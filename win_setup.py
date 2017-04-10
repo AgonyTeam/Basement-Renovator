2d4bf761efedafde311435c11716f23c69f6c6f4
dedChar <unnamed0736@gmail.com>
2017-04-10 20:09:41 +0200

    Updated win_setup.py * Fixed icon and other file properties * Most python related files are stored inside the executable now

--- win_setup.py
+++ win_setup.py
@@ -6,22 +6,26 @@ sys.argv.append('py2exe')
 if os.path.exists('dist'):
 	shutil.rmtree('dist')
 
+with open('.git/refs/heads/master') as file:
+	commithash = file.readline().strip()
+	
 setup(
-	name="Basement Renovator",
-	version="0.1",
-	author="Tempus",
-	author_email="Tempus@chronometry.ca",
-	windows = [{'script': "BasementRenovator.py"}],
+	windows = [{
+		'script': "BasementRenovatorAfterbirth.py",
+		'icon_resources': [(0, "resources/UI/BasementRenovator.ico")],
+		'product_name': "Basement Renovator",
+		'version': "3",
+		'description': "A better room editor for The Binding of Isaac: Afterbirth+",
+		'company_name': "Tempus",
+	}],
 	options = {'py2exe': {
 		'bundle_files': 1, 
-		'compressed': False, 
-		"includes":["sip", "xml.etree"],
+		'compressed': True, 
+		'includes':["sip", "xml.etree", "psutil", "PyQt5"],
 		'optimize': 2,
-		'bundle_files': 3,
-		"icon_resources": [(0, "resources/UI/BasementRenovator.ico")]
-	}}
+	}},
+	zipfile = None
 )
-
 
 os.mkdir('dist/platforms')
 shutil.copytree('resources', 'dist/resources')
